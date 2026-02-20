445be8118fe5574e15d80fcc0c7442d5812cf98a
Chris Corbell <88693878+chriscorbell@users.noreply.github.com>
2026-02-20 00:09:56 -0500

    Add uv installation command to windows.ps1
    Added installation command for uv in PowerShell script.

--- windows.ps1
+++ windows.ps1
@@ -38,5 +38,8 @@ wsl --install --no-distribution
 # Install Bun
 powershell -c "irm bun.sh/install.ps1|iex"
 
+# Install uv
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+
 # Restore classic right-click menu
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
