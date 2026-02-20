f537006845ac576ebeeaa394235eae7d64f264e4
Chris Corbell <88693878+chriscorbell@users.noreply.github.com>
2026-02-19 23:30:19 -0500

    Add Bun installation command to windows.ps1
    Added installation command for Bun using PowerShell.

--- windows.ps1
+++ windows.ps1
@@ -35,5 +35,8 @@ winget install Microsoft.PowerToys
 winget install Adobe.CreativeCloud
 wsl --install --no-distribution
 
+# Install Bun
+powershell -c "irm bun.sh/install.ps1|iex"
+
 # Restore classic right-click menu
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
